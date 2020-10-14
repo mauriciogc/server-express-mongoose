d2ad041cb4cd950c597b6f5c99f95578b4b5e185
mauriciogc <mauriciogc@gmail.com>
2020-10-14 16:44:57 -0500

    add models and routers

---
+++ models/messages.js
@@ -0,0 +1,12 @@
+let messages = {
+  1: {
+    id: 1,
+    post: "Ad occaecat dolore velit nulla exercitation in aute mollit nulla.",
+  },
+  2: {
+    id: 2,
+    post: "Occaecat consequat in occaecat sit elit ea ex id esse ipsum.",
+  },
+};
+
+export default messages;
--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 import "dotenv/config";
-import { v4 as uuidv4 } from "uuid";
 const express = require("express");
 const cors = require("cors");
 
-//data
-let messages = {
-  1: {
-    id: 1,
-    post: "Ad occaecat dolore velit nulla exercitation in aute mollit nulla.",
-  },
-  2: {
-    id: 2,
-    post: "Occaecat consequat in occaecat sit elit ea ex id esse ipsum.",
-  },
-};
+import models from "../models";
+import routes from "../routes";
 
 const app = express();
 const port = process.env.PORT;
@@ -21,39 +11,16 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Add cors
 app.use(cors());
 
-app.get("/messages", (req, res) => {
-  return res.send(messages);
-});
-
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post("/messages", (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    message: req.body.message,
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
   };
-
-  messages[id] = message;
-
-  return res.send(message);
+  next();
 });
 
-app.delete("/messages/:messageId", (req, res) => {
-  const message = messages[req.params.messageId];
-  delete messages[req.params.messageId];
-
-  //another way
-  /*const { [req.params.messageId]: message, ...otherMessages } = messages;
-  messages = otherMessages;*/
-
-  return res.send(message);
-});
+app.use("/messages", routes.messages);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
