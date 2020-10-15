8ddf5fd5acb4a5a250ed9c359061d0182aa04362
mauriciogc <mauriciogc@gmail.com>
2020-10-15 16:59:47 -0500

    remove comments

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
-//var ObjectId = require("mongodb").ObjectId;
 
 const router = Router();
 
@@ -30,40 +29,5 @@ router.delete("/:messageId", (req, res) => {
 
   return res.send(message);
 });
-
-/* router.get("/", async (req, res) => {
-  const messages = await req.context.collections.messages.find().toArray();
-  return res.send(messages);
-});
-
-router.get("/:messageId", async (req, res) => {
-  const message = await req.context.collections.messages
-    .find(ObjectId(req.params.messageId))
-    .toArray();
-
-  return res.send(message);
-});
-
-router.post("/", async (req, res) => {
-  const newMessage = {
-    message: req.body.message,
-  };
-
-  const message = await req.context.collections.messages.insertOne(newMessage);
-
-  return res.send(message.ops[0]);
-});
-
-router.delete("/:messageId", async (req, res) => {
-  const message = await req.context.collections.messages.deleteOne({
-    _id: new ObjectId(req.params.messageId),
-  });
-
-  if (message.deletedCount === 1) {
-    return res.send({});
-  }
-
-  return res.send({ message: "No se ha borrado" });
-}); */
 
 export default router;
