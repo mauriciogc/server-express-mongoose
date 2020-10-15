8518210cfddbc1343f38f3135c8c20b632931ba3
mauriciogc <mauriciogc@gmail.com>
2020-10-15 16:58:55 -0500

    add 0.2.0 blog2

--- routes/messages.js
+++ routes/messages.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
+//var ObjectId = require("mongodb").ObjectId;
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send(req.context.models.messages);
+  return res.send(req.context.messages);
 });
 
 router.get("/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
+  return res.send(req.context.messages[req.params.messageId]);
 });
 
 router.post("/", (req, res) => {
@@ -18,16 +19,51 @@ router.post("/", (req, res) => {
     message: req.body.message,
   };
 
-  req.context.models.messages[id] = message;
+  req.context.messages[id] = message;
 
   return res.send(message);
 });
 
 router.delete("/:messageId", (req, res) => {
-  const message = req.context.models.messages[req.params.messageId];
-  delete req.context.models.messages[req.params.messageId];
+  const message = req.context.messages[req.params.messageId];
+  delete req.context.messages[req.params.messageId];
 
   return res.send(message);
 });
+
+/* router.get("/", async (req, res) => {
+  const messages = await req.context.collections.messages.find().toArray();
+  return res.send(messages);
+});
+
+router.get("/:messageId", async (req, res) => {
+  const message = await req.context.collections.messages
+    .find(ObjectId(req.params.messageId))
+    .toArray();
+
+  return res.send(message);
+});
+
+router.post("/", async (req, res) => {
+  const newMessage = {
+    message: req.body.message,
+  };
+
+  const message = await req.context.collections.messages.insertOne(newMessage);
+
+  return res.send(message.ops[0]);
+});
+
+router.delete("/:messageId", async (req, res) => {
+  const message = await req.context.collections.messages.deleteOne({
+    _id: new ObjectId(req.params.messageId),
+  });
+
+  if (message.deletedCount === 1) {
+    return res.send({});
+  }
+
+  return res.send({ message: "No se ha borrado" });
+}); */
 
 export default router;
--- src/app.js
+++ src/app.js
@@ -1,9 +1,61 @@
+/* import "dotenv/config";
+const express = require("express");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+
+//import models from "../models";
+import routes from "../routes";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+const flags = { useNewUrlParser: true, useUnifiedTopology: true };
+const urlDB = "mongodb://127.0.0.1:27017/";
+MongoClient.connect(urlDB, flags, (err, client) => {
+  if (err) return console.error(err);
+  console.log("Connected to Database");
+  const db = client.db("express-test-2");
+  const collections = {
+    messages: db.collection("messages"),
+  };
+
+  app.use(async (req, res, next) => {
+    req.context = {
+      collections,
+    };
+    next();
+  });
+
+  app.use("/messages", routes.messages);
+});
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+ */
+
 import "dotenv/config";
+import { v4 as uuidv4 } from "uuid";
 const express = require("express");
 const cors = require("cors");
 
-import models from "../models";
-import routes from "../routes";
+import routes from "../routes"; //(A)
+
+let messages = {
+  1: {
+    id: 1,
+    message:
+      "Ad occaecat ad dolore velit nulla exercitation in aute mollit nulla.",
+  },
+  2: {
+    id: 2,
+    message: "Occaecat consequat in occaecat sit elit ea ex id esse ipsum.",
+  },
+};
 
 const app = express();
 const port = process.env.PORT;
@@ -15,12 +67,12 @@ app.use(cors());
 
 app.use(async (req, res, next) => {
   req.context = {
-    models,
+    messages,
   };
   next();
 });
 
-app.use("/messages", routes.messages);
+app.use("/messages", routes.messages); //(B)
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
